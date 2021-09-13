b866366908e1a0e0160161af050cb3046004e0b9
MadJangE00 <jiniixz@naver.com>
2021-09-13 12:40:50 +0900

    day02 - 2

--- Day02/day02/app.js
+++ Day02/day02/app.js
@@ -2,8 +2,8 @@ const input = document.querySelector('.theme-switcher input');
 
 input.addEventListener('change', (e) => {
   if (e.target.checked) {
-    document.body.setAttribute("data-theme", "dark");
+    document.body.setAttribute('data-theme', 'dark');
   } else {
-    document.body.setAttribute("data-theme", "light");
+    document.body.setAttribute('data-theme', 'light');
   }
 })
