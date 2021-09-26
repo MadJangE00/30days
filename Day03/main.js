fa575f510c5aad9cb4297d4595616a7b3f28baf9
JangE <mad.jange@gmail.com>
2021-09-27 01:36:08 +0900

    day3 finish

--- Day03/main.js
+++ Day03/main.js
@@ -1,18 +1,18 @@
 const button = document.querySelector(".container button");
 const jokeDiv = document.querySelector(".container .joke p");
 
-document.addEventListener("DOMContentLoaded", getJock);
+document.addEventListener("DOMContentLoaded", getJoke);
 
-button.addEventListener("click", getJock);
+button.addEventListener("click", getJoke);
 
-function getJoke(){
-    fetch('https://icanhazdadjoke.com/', {
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    .then(data => data.json())
-    .then(obj => jokeDiv.innerHTML = obj.joke);
+function getJoke() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((data) => data.json())
+    .then((obj) => (jokeDiv.innerHTML = obj.joke));
 }
 
 // async function getJock() {
